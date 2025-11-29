7b071793c0ec1ef5c4ac4f7b41159d9a91fffe2b
Zohaib-bhai-developer <shakeelzohaib99@gmail.com>
2025-11-29 22:58:55 +0500

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -3,7 +3,7 @@ const Replicate = require("replicate");
 module.exports = async (req, res) => {
   try {
     if (req.method !== "POST") {
-      return res.status(405).json({ error: "Only POST allowed" });
+      return res.status(405).json({ error: "Only POST requests allowed" });
     }
 
     const { prompt } = req.body;
@@ -17,12 +17,15 @@ module.exports = async (req, res) => {
 
     const output = await replicate.run(
       "black-forest-labs/flux-schnell",
-      { input: { prompt } }
+      {
+        input: { prompt }
+      }
     );
 
     return res.status(200).json({ image: output[0] });
 
   } catch (err) {
+    console.error("Error:", err);
     return res.status(500).json({ error: err.message });
   }
 };
