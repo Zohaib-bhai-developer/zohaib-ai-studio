0d5c485360a9d3257b1af7db322f0175534e0c26
Zohaib-bhai-developer <shakeelzohaib99@gmail.com>
2025-11-29 22:34:54 +0500

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -9,7 +9,8 @@ export default async function handler(req, res) {
     }
 
     const replicate = new Replicate({
-      auth: process.env.REPLICATE_API_TOKEN,
+      auth: process.env.REPLICATE_API_KEY,
+      
     });
 
     const output = await replicate.run(
